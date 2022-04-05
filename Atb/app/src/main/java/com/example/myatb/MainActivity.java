e1843d43df344f93ff467c2fc6fc9d55ca483a70
Андрій Кондрацький <Nashemistoart@gmail.com>
2022-04-05 12:13:25 +0300

    Asp context

--- Atb/app/src/main/java/com/example/myatb/MainActivity.java
+++ Atb/app/src/main/java/com/example/myatb/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.myatb;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView tvInfo;
+    private EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvInfo=findViewById(R.id.tvInfo);
+        editText=findViewById(R.id.editTextData);
+    }
+
+    public void  handleClick(View view){
+        String text=editText.getText().toString();
+        tvInfo.setText(text);
     }
 }
