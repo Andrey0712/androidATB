2ff4a9d52c4d6f69aef476039628368c1be619dd
Андрій Кондрацький <Nashemistoart@gmail.com>
2022-04-07 12:41:40 +0300

    error register

--- Atb/app/src/main/java/com/example/myatb/MainActivity.java
+++ Atb/app/src/main/java/com/example/myatb/MainActivity.java
@@ -10,6 +10,9 @@ import android.widget.TextView;
 import com.example.myatb.network.account.AccountService;
 import com.example.myatb.network.account.dto.AccountResponseDTO;
 import com.example.myatb.network.account.dto.RegisterDto;
+import com.example.myatb.network.account.dto.RegisterErrorDTO;
+import com.example.myatb.network.account.dto.ValidationRegisterDTO;
+import com.google.gson.Gson;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -39,8 +42,21 @@ public class MainActivity extends AppCompatActivity {
                 .enqueue(new Callback<AccountResponseDTO>() {
                     @Override
                     public void onResponse(Call<AccountResponseDTO> call, Response<AccountResponseDTO> response) {
-                        AccountResponseDTO data= response.body();
-                        tvInfo.setText("response is good");
+                        if(response.isSuccessful()){
+                            AccountResponseDTO data= response.body();
+                            tvInfo.setText("response is good");
+                        }
+                        try {
+                            String json=response.errorBody().string();
+                            Gson gson=new Gson();
+                            ValidationRegisterDTO result =gson.fromJson(json, ValidationRegisterDTO.class);
+                            RegisterErrorDTO registerErrorDTO=(RegisterErrorDTO) result.getErrors();
+                            int t=3;
+
+                        }catch (Exception e){
+                            System.out.println("Error response parse body");
+                        }
+
                     }
 
 
---
+++ Atb/app/src/main/java/com/example/myatb/network/account/dto/RegisterErrorDTO.java
@@ -0,0 +1,10 @@
+package com.example.myatb.network.account.dto;
+
+import lombok.Data;
+
+@Data
+    public class RegisterErrorDTO {
+        private String[] email;
+        //private String[] firstName;
+    }
+
